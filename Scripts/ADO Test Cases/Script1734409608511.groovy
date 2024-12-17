2a740a2ede893ce54577963d3eb7ed774f8064e0
manhuakatalon <man.hua@katalon.com>
2024-12-17 11:57:40 +0700

    add

--- Scripts/ADO Test Cases/Script1734409608511.groovy
+++ Scripts/ADO Test Cases/Script1734409608511.groovy
@@ -12,8 +12,19 @@ WebUI.comment('Given that the user has logged into their account')
 
 WebUI.openBrowser(GlobalVariable.G_SiteURL)
 
-WebUI.callTestCase(findTestCase('null'), [('Username') : 'John Doe', ('Password') : 'ThisIsNotAPassword'], 
-    FailureHandling.STOP_ON_FAILURE)
+WebUI.click(findTestObject('Page_CuraHomepage/btn_MakeAppointment'))
+
+WebUI.setText(findTestObject('Page_Login/txt_UserName'), 'John Doe')
+
+WebUI.setText(findTestObject('Page_Login/txt_Password'), 'ThisIsNotAPassword')
+
+WebUI.comment('When he logins to CURA system')
+
+WebUI.click(findTestObject('Page_Login/btn_Login'))
+
+WebUI.comment('Then he should be able to login successfully')
+
+landingPage = WebUI.verifyElementPresent(findTestObject('Page_CuraAppointment/div_Appointment'), GlobalVariable.G_Timeout)
 
 WebUI.comment('And Appointment page is displayed')
 
